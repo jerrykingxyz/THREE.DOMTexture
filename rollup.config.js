9bd2f06118b8c662f032bb42319d3e19c6d175be
jerrykingxyz <jerrykingxyz@gmail.com>
2020-04-25 20:49:03 +0800

    feat: esm support

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,56 @@
-const keyword = function () {
+import uglify from "rollup-plugin-uglify";
+import pkg from "./package.json";
+
+const keyword = function() {
   return {
-    transform (code, id) {
-      if ( /\.js$/.test( id ) === false ) return;
+    transform(code, id) {
+      if (/\.js$/.test(id) === false) return;
 
       return {
-        code: code.replace(/let /g, 'var ').replace(/const /g, 'var ')
-      }
+        code: code.replace(/let /g, "var ").replace(/const /g, "var ")
+      };
     }
-  }
-}
-
-export default {
-  input: 'src/index.js',
-  indent: '\t',
-  external: ['three'],
-  output: {
-    file: './dist/DOMTexture.js',
-    format: 'umd',
-    name: 'DOMTexture',
-  },
-  globals: {
-    'three': 'THREE',
-  },
-  watch: {
-    include: 'src/**',
-  },
-  plugins: [
-    keyword()
-  ]
+  };
 };
+
+const common = {
+  input: "src/index.js",
+  indent: "\t",
+  external: ["three"],
+  globals: {
+    three: "THREE"
+  }
+};
+
+export default [
+  // nodejs & browsers
+  {
+    ...common,
+    output: {
+      name: "DOMTexture",
+      file: pkg.main,
+      format: "umd"
+    },
+    plugins: [keyword()]
+  },
+  // browsers min
+  {
+    ...common,
+    output: {
+      name: "DOMTexture",
+      file: pkg.browser,
+      format: "umd"
+    },
+    plugins: [keyword(), uglify()]
+  },
+  // mjs
+  {
+    ...common,
+    output: {
+      name: "DOMTexture",
+      file: pkg.module,
+      format: "es"
+    },
+    plugins: [keyword()]
+  }
+];
